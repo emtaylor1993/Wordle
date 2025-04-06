4dcab46c1d835040a0730b78093ed03a73558f71
etaylor5 <etaylorsubs1993@gmail.com>
2025-04-05 22:14:42 -0400

    Fixed comments in scripts.

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -3,6 +3,6 @@ echo Starting and Building Docker Containers...
 cd ..
 docker compose --env-file ./wordle-backend/.env up --build
 
-## Use this to perform a healthcheck.
-# docker inspect --format='{{json .State.Health}}' wordle-backend
-# docker inspect --format='{{json .State.Health}}' wordle-frontend
+:: Use this to perform a healthcheck.
+:: docker inspect --format='{{json .State.Health}}' wordle-backend
+:: docker inspect --format='{{json .State.Health}}' wordle-frontend
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,3 +3,7 @@
 echo "Starting and Building Docker Containers..."
 cd ..
 docker compose --env-file ./wordle-backend/.env up --build
+
+# Use this to perform a healthcheck.
+# docker inspect --format='{{json .State.Health}}' wordle-backend
+# docker inspect --format='{{json .State.Health}}' wordle-frontend
